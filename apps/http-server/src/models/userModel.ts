079fd26726450add1ae8faf41fe8a1c401aba421
Shiv Shukla <shivshukla00514@gmail.com>
2026-08-22 10:29:22 +0530

    add: user model

---
+++ apps/http-server/src/models/userModel.ts
@@ -0,0 +1,56 @@
+import mongoose from "mongoose";
+
+export interface IUser {
+    _id: mongoose.Types.ObjectId,
+    firstName: string,
+    lastName: string,
+    email: string,
+    password: string,
+    city: string,
+    country: string,
+    phoneNumber: string,
+};
+
+const userSchema = new mongoose.Schema<IUser> ({
+    firstName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    lastName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    city: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    country: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    phoneNumber: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+}, { timestamps: true });
+
+export const User = mongoose.models.User as mongoose.Model<IUser> || mongoose.model<IUser>("user", userSchema);
